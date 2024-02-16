8809abf0571f90145ac5c67ff2664784d2b4c5ea
Jacob Husband <jakehusband2@gmail.com>
2024-02-16 06:14:10 -0800

    add sumrestext

--- GMEPElectricalCommands/GeneralCommands.cs
+++ GMEPElectricalCommands/GeneralCommands.cs
@@ -416,121 +416,121 @@ namespace ElectricalCommands
       }
     }
 
-        [CommandMethod("SUMRESTEXT")]
-        public void SUMRESTEXT()
+    [CommandMethod("SUMRESTEXT")]
+    public void SUMRESTEXT()
+    {
+      var (doc, db, ed) = GeneralCommands.GetGlobals();
+
+      PromptSelectionResult selection = ed.SelectImplied();
+
+      if (selection.Status != PromptStatus.OK)
+      {
+        PromptSelectionOptions opts = new PromptSelectionOptions();
+        opts.MessageForAdding = "Select text objects to sum: ";
+        opts.AllowDuplicates = false;
+        opts.RejectObjectsOnLockedLayers = true;
+
+        selection = ed.GetSelection(opts);
+        if (selection.Status != PromptStatus.OK)
+          return;
+      }
+
+      double sum = 0.0;
+
+      using (Transaction tr = db.TransactionManager.StartTransaction())
+      {
+        foreach (SelectedObject so in selection.Value)
         {
-            var (doc, db, ed) = GeneralCommands.GetGlobals();
+          DBText text = tr.GetObject(so.ObjectId, OpenMode.ForRead) as DBText;
+          MText mtext = tr.GetObject(so.ObjectId, OpenMode.ForRead) as MText;
 
-            PromptSelectionResult selection = ed.SelectImplied();
+          if (text != null)
+          {
+            double value;
+            string textString = text
+                .TextString.Replace("\\FArial;", "")
+                .Replace("\\L", "")
+                .Replace("\\Farial|c0;", "")
+                .Replace("{", "")
+                .Replace("}", "")
+                .Replace("\\I", "")
+                .Trim();
 
-            if (selection.Status != PromptStatus.OK)
+            textString = new string(
+                textString.Where(c => char.IsDigit(c) || c == '.').ToArray()
+            );
+
+            if (textString.Contains("VA"))
             {
-                PromptSelectionOptions opts = new PromptSelectionOptions();
-                opts.MessageForAdding = "Select text objects to sum: ";
-                opts.AllowDuplicates = false;
-                opts.RejectObjectsOnLockedLayers = true;
-
-                selection = ed.GetSelection(opts);
-                if (selection.Status != PromptStatus.OK)
-                    return;
-            }
-
-            double sum = 0.0;
-
-            using (Transaction tr = db.TransactionManager.StartTransaction())
-            {
-                foreach (SelectedObject so in selection.Value)
+              string[] sections = textString.Split(
+                  new string[] { "VA" },
+                  StringSplitOptions.None
+              );
+              foreach (string section in sections)
+              {
+                double num = 0;
+                if (double.TryParse(section, out num))
                 {
-                    DBText text = tr.GetObject(so.ObjectId, OpenMode.ForRead) as DBText;
-                    MText mtext = tr.GetObject(so.ObjectId, OpenMode.ForRead) as MText;
-
-                    if (text != null)
-                    {
-                        double value;
-                        string textString = text
-                            .TextString.Replace("\\FArial;", "")
-                            .Replace("\\L", "")
-                            .Replace("\\Farial|c0;", "")
-                            .Replace("{", "")
-                            .Replace("}", "")
-                            .Replace("\\I", "")
-                            .Trim();
-
-                        textString = new string(
-                            textString.Where(c => char.IsDigit(c) || c == '.').ToArray()
-                        );
-
-                        if (textString.Contains("VA"))
-                        {
-                            string[] sections = textString.Split(
-                                new string[] { "VA" },
-                                StringSplitOptions.None
-                            );
-                            foreach (string section in sections)
-                            {
-                                double num = 0;
-                                if (double.TryParse(section, out num))
-                                {
-                                    sum += num;
-                                }
-                            }
-                        }
-                        if (Double.TryParse(textString, out value))
-                            sum += value;
-                    }
-                    else if (mtext != null)
-                    {
-                        double value;
-                        string mTextContents = mtext
-                            .Contents.Replace("\\FArial;", "")
-                            .Replace("\\L", "")
-                            .Replace("\\Farial|c0;", "")
-                            .Replace("{", "")
-                            .Replace("}", "")
-                            .Replace("\\I", "")
-                            .Trim();
-
-                        if (mTextContents.Contains("VA"))
-                        {
-                            string[] sections = mTextContents.Split(
-                                new string[] { "VA" },
-                                StringSplitOptions.None
-                            );
-                            foreach (string section in sections)
-                            {
-                                double num = 0;
-                                var newSection = new string(
-                                    section.Where(c => char.IsDigit(c) || c == '.').ToArray()
-                                );
-                                if (double.TryParse(newSection, out num))
-                                {
-                                    sum += num;
-                                }
-                            }
-                        }
-                        else if (Double.TryParse(mTextContents, out value))
-                        {
-                            sum += value;
-                        }
-                        else
-                        {
-                            mTextContents = new string(
-                                mTextContents.Where(c => char.IsDigit(c) || c == '.').ToArray()
-                            );
-                            if (Double.TryParse(mTextContents, out value))
-                                sum += value;
-                        }
-                    }
+                  sum += num;
                 }
-                ed.WriteMessage($"\nThe sum of selected text objects is: {sum}");
-                tr.Commit();
+              }
             }
-        }
+            if (Double.TryParse(textString, out value))
+              sum += value;
+          }
+          else if (mtext != null)
+          {
+            double value;
+            string mTextContents = mtext
+                .Contents.Replace("\\FArial;", "")
+                .Replace("\\L", "")
+                .Replace("\\Farial|c0;", "")
+                .Replace("{", "")
+                .Replace("}", "")
+                .Replace("\\I", "")
+                .Trim();
 
-        [CommandMethod("AREACALCULATOR")]
-        public void AREACALCULATOR()
-        {
-            var (doc, _, ed) = GeneralCommands.GetGlobals();
+            if (mTextContents.Contains("VA"))
+            {
+              string[] sections = mTextContents.Split(
+                  new string[] { "VA" },
+                  StringSplitOptions.None
+              );
+              foreach (string section in sections)
+              {
+                double num = 0;
+                var newSection = new string(
+                    section.Where(c => char.IsDigit(c) || c == '.').ToArray()
+                );
+                if (double.TryParse(newSection, out num))
+                {
+                  sum += num;
+                }
+              }
+            }
+            else if (Double.TryParse(mTextContents, out value))
+            {
+              sum += value;
+            }
+            else
+            {
+              mTextContents = new string(
+                  mTextContents.Where(c => char.IsDigit(c) || c == '.').ToArray()
+              );
+              if (Double.TryParse(mTextContents, out value))
+                sum += value;
+            }
+          }
+        }
+        ed.WriteMessage($"\nThe sum of selected text objects is: {sum}");
+        tr.Commit();
+      }
+    }
+
+    [CommandMethod("AREACALCULATOR")]
+    public void AREACALCULATOR()
+    {
+      var (doc, _, ed) = GeneralCommands.GetGlobals();
 
       PromptSelectionOptions opts = new PromptSelectionOptions();
       opts.MessageForAdding = "Select polylines or rectangles: ";
