b42281ec754c1c9e2f8416b60a438461358b724e
Jacob Husband <jacobhusbandwork@gmail.com>
2024-03-05 10:24:38 -0800

    Complete setupxref command

--- GMEPElectricalCommands/XrefCommands.cs
+++ GMEPElectricalCommands/XrefCommands.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
 using System;
 using System.Collections.Generic;
